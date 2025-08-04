5b1b9ac129c2386a9d483b244a3fcf8eade95c62
Moo-Jie <drc9941@outlook.com>
2025-08-04 12:49:31 +0800

    feat:初始化后端项目。

---
+++ src/main/java/com/rich/richcodeweaver/RichCodeWeaverApplication.java
@@ -0,0 +1,13 @@
+package com.rich.richcodeweaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RichCodeWeaverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RichCodeWeaverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rich/richcodeweaver/RichCodeWeaverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rich.richcodeweaver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RichCodeWeaverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
