18e10b4170a30d9c88bb479e5c403255500b652a
Moo-Jie <drc9941@outlook.com>
2025-08-31 11:00:36 +0800

    feat：引入基于 x-file-storage 的文件上传接口。

--- src/main/java/com/rich/richcodeweaver/RichCodeWeaverApplication.java
+++ src/main/java/com/rich/richcodeweaver/RichCodeWeaverApplication.java
@@ -1,9 +1,12 @@
 package com.rich.richcodeweaver;
 
+import cn.xuyanwu.spring.file.storage.spring.EnableFileStorage;
 import dev.langchain4j.community.store.embedding.redis.spring.RedisEmbeddingStoreAutoConfiguration;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// 开启对象存储
+@EnableFileStorage
 // 排除 Embedding 的自动配置，后续使用 RAG 时再开启
 @SpringBootApplication(exclude = {RedisEmbeddingStoreAutoConfiguration.class})
 @MapperScan("com.rich.richcodeweaver.mapper")
